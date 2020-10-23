72f6b88e2c1d759febf25973fecfeffa7264fd4c
hafizrizqi21 <hafiz.rizqi@gmail.com>
2020-10-23 20:57:29 +0700

    Upload Project Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroCarousel />
+    <ProductCarousel />
+    <InstaShayna />
+    <PartnerLogo />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna";
+import HeroCarousel from "@/components/HeroCarousel";
+import ProductCarousel from "@/components/ProductCarousel";
+import InstaShayna from "@/components/InstaShayna";
+import PartnerLogo from "@/components/PartnerLogo";
+import FooterShayna from "@/components/FooterShayna";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroCarousel,
+    ProductCarousel,
+    InstaShayna,
+    PartnerLogo,
+    FooterShayna,
+  },
+};
+</script>
